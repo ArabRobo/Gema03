e032d4a0f97b730ea78b5227df3b3bbac6a56fd7
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-06-02 23:17:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "26730559"))
 API_HASH = os.environ.get("API_HASH", "54e0fd326f54b4ea91fdcbdf98e3cf4e")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001553869235"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002169125532"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Dhilnihnge")
@@ -39,7 +39,7 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://vjoubnyo:KZWPg-M9TznS-nt4wLou89VxT3HP-jLI@suleiman.db.elephantsql.com/vjoubnyo")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002196255155"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002106183738"))
 
 # Tulisan joinnya mau gimana
